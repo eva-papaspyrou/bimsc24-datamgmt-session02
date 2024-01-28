02a6229cfcd2b10564074313bece59c1933d58b9
eva-papaspyrou <evangelia.papaspyrou@students.iaac.net>
2024-01-28 19:29:40 +0200

    HTML and CSS in app.vue

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,39 @@ import { ref } from "vue" // you need this to use ref()
 <!-- the template is where the html code goes -->
 <template>
 
+<div id="navbar" class="container"></div>
 
+<div id="flex">
+    <div id="empty" class="container"></div>
+    <div id="main" class="container">main content
+
+        <div id="video-container">
+            <video controls 
+            width="1120" 
+            height="630"
+            autoplay
+            loop
+            >
+                <source src="https://cdn-luma.com/69340914fb394511ff09152f1a3d77041b1354ce75151412ab93b41e7d208c7e.mp4" type="video/mp4">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    </div>
+    <div id="sidebar" class="container">
+        <div id="image" class="sidebar-item">
+            <img src="https://w7.pngwing.com/pngs/1007/856/png-transparent-white-and-black-cow-cattle-sheep-calf-goat-live-cow-animals-cow-goat-family-snout-thumbnail.png">
+        </div>
+        <div id="text1" class="sidebar-item">
+            <p>home</p>
+        </div>
+        <div id="text2" class="sidebar-item">
+            <p>projects</p>
+        </div>
+        <div id="text3" class="sidebar-item">
+            <p>learn</p>
+        </div>
+    </div>
+</div>
 
 </template>
 
@@ -19,5 +51,99 @@ import { ref } from "vue" // you need this to use ref()
 <!-- style is where the css code goes -->
 <style scoped>
 
+/* VIDEO */
+/* size */
+#video-container {
+    position: fixed; /* Use fixed positioning to cover the entire viewport */
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1; /* Set a negative z-index to place it behind other elements */
+}
+
+#video-container video {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    opacity: 0.6;
+}
+/* Disable native controls in WebKit browsers */
+#video-container video::-webkit-media-controls {
+    display: none !important; 
+}
+
+html {
+    background-color: rgb(241, 241, 241);
+    color: rgb(255, 255, 255);
+    font-family: Helvetica, sans-serif;
+    font-weight: bold;
+    font-size: 27px;
+}
+
+body {
+    background-image: url('https://raw.githubusercontent.com/jose-villegas/HeightmapGenerator/master/HeightmapGenerator/Resources/perlin_1.BMP');
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    margin: 0;
+}
+
+div {
+    box-sizing: border-box;
+}
+
+#navbar {
+    height: 100px;
+}
+
+#flex {
+    display: flex;
+    height: calc(100vh - 100px);
+}
+#empty{
+    width: 15%;
+}
+#main {
+    width: 80%;
+    position: relative;
+    z-index: 1; /* Set a higher z-index to place it in front of the video */
+}
+/* VIDEO */
+/* size */
+#sidebar {
+    width: 5%;
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+    align-items: center;
+    z-index: 1; /* Set a higher z-index to place it in front of the video */
+}
+
+.sidebar-item {
+    margin-bottom: 50px;
+}
+
+.sidebar-item p {
+    transform: rotate(90deg);
+    white-space: nowrap;
+    overflow: hidden;
+    transform-origin: 0 5;
+    color: white; /* Set text color to white */
+  font-family: Helvetica, sans-serif; /* Set font-family to Helvetica */
+  font-size: 27px;
+  font-weight: bold;
+}
+
+.sidebar-item img {
+    width: 40px; /* Adjust the width of the small icon */
+    height: 50px; /* Adjust the height of the small icon */
+}
+
+/* .container {
+    border-color: rgb(60, 255, 0);
+    border-style: dotted;
+    border-width: 1px;
+} */
 
 </style>
